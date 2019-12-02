387370eafa076f04345bee6655446b5a214beb61
Raluca Iordan <raiordan@deloittece.com>
2019-12-02 14:40:21 +0200

    Initial Commit

---
+++ FashionStores/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FashionStores
+//
+//  Created by Iordan, Raluca on 02/12/2019.
+//  Copyright © 2019 Iordan, Raluca. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
